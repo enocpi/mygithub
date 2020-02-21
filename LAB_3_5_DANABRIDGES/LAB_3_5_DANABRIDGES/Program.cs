6865d0b0abf3b0e47e965fa99d8de95a9788fcbf
enocpi <61128286+enocpi@users.noreply.github.com>
2020-02-20 18:51:50 -0600

    Add files via upload

---
+++ LAB_3_5_DANABRIDGES/LAB_3_5_DANABRIDGES/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace LAB_3_5_DANABRIDGES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blacklist = "follow me";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            if (message.Contains(blacklist))
+            {
+                isSpam = true;
+                Console.WriteLine("this is spam");
+            }
+
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("this isnt spam");
+
+
+            }
+
+
+            
+
+
+
+            
+          
+           
+                   
+           
+           
+          
+           
+            
+
+            
+            
+                
+            
+
+                 
+
+
+
+
+        }
+ 
+    }
+}
+git remote add origin https://github.com/enocpi/mygithub.git
+git push -u origin maste
+
